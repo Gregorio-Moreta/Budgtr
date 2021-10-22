6105edcc667f97ee73a7d92b82b5771d2c9e9b6f
Gregorio Moreta <gregoriomoreta4@gmail.com>
2021-10-22 06:30:51 -0400

    I got the new route working. Now I can add a new item and have it posted back onto the index page and joi the rest of the budget items. Next is adding an edit option.

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 //  Import my budget data
 const budget = require("./models/budget")
+// Import method override 
+const methodOverride = require('method-override')
 
 //////////////////
 //  MIDDLEWARE
@@ -17,8 +19,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 //after app has been defined
 //use methodOverride.  We'll be adding a query parameter to our delete form named _method
-// app.use(methodOverride("_method"));
+app.use(methodOverride("_method"));
 
+// app.use((req, res, next) => {
+//     console.log('I run for all routes');
+//     next();
+// });
 
 //////////////////
 //  ROUTES
@@ -27,6 +33,18 @@ app.use(express.static("public"));
 //  Index Route 
 app.get('/budget', (req, res) => {
     res.render('index.ejs', {Budget: budget})
+})
+//  New Route
+app.get('/budget/new', (req, res) => {
+    res.render('new.ejs', {Budget: budget, title: "new"});
+});
+//  Post route for the previously created New Route above
+app.post('/budget', (req, res) => {
+    console.log(req.body)
+    budget.push(req.body)
+    // console.log(budget)
+    res.redirect('/budget')
+    console.log(budget)
 })
 //  Show route
 app.get('/budget/:indexOfBudget', (req, res) => {
