bdafa5e8e3408c05c36d0da4cc21501990d243e6
Gregorio Moreta <gregoriomoreta4@gmail.com>
2021-10-22 01:07:11 -0400

    I got the show route working. A new route is next.

--- server.js
+++ server.js
@@ -9,8 +9,16 @@ const budget = require("./models/budget")
 //  MIDDLEWARE
 //////////////////
 
+// near the top, around other app.use() call
+// extended property is a body parser
+// Parse Request Bodies if Content-Type Header is: application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
 //  Serve files statically from the public folder
-app.use(express.static("public"))
+app.use(express.static("public"));
+//after app has been defined
+//use methodOverride.  We'll be adding a query parameter to our delete form named _method
+// app.use(methodOverride("_method"));
+
 
 //////////////////
 //  ROUTES
@@ -20,7 +28,10 @@ app.use(express.static("public"))
 app.get('/budget', (req, res) => {
     res.render('index.ejs', {Budget: budget})
 })
-
+//  Show route
+app.get('/budget/:indexOfBudget', (req, res) => {
+    res.render('show.ejs', {Budget: budget[req.params.indexOfBudget], title: "show"})
+})
 
 app.listen(9000, () => {
     console.log('listening')
